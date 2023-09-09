d81fa8b42904bf7f6f06060a1ecdf4be3bf7340b
Laura_1998 <laurasaldel24@gmail.com>
2023-09-09 15:12:33 -0500

    cambios generales

--- js/functions.js
+++ js/functions.js
@@ -4,7 +4,7 @@ export function get_date(number_day, number_month, number_year, week_day) {
     const date = new Date();
     const days = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
     let day = `${date.getDate()}`;
-    let month = `${date.getMonth()}`
+    let month = `${date.getMonth() + 1}`
     let year = `${date.getFullYear()}`
 
     // condiciones para validar que se agreguen dos dígitos en el número de día y mes
@@ -32,11 +32,10 @@ export function validate_empty_fields(field) {
     }
 }
 
-let array_local;
-let array_new_task = [];
+let array_local; // variable que contendrá el array traido del localstorage
 
 /* función añadir tarea */
-export function add_task(input_add_task) {
+export function add_task(input_add_task, icon_inform, message_modal_inform, container_modal_inform) {
     // Se trae el array guardado en el localstorage si no existe se guarda la variable como un array vacío
     array_local = JSON.parse(localStorage.getItem("tasks")) || [];
 
@@ -46,29 +45,60 @@ export function add_task(input_add_task) {
         estatus: false,
         id: input_add_task.value.replaceAll(" ", "").toLowerCase(),
     }
-    // se hace la condición para verificar si el array ya existe en el localstorage
-    // si ya existe se agrega el nuevo elemento al array
-    if (array_local.length > 0) {
-        // se busca en el array si ya existe alguna tarea igual a la que se quiere agregar
-        let task_find = array_local.find((task) => object_task.id === task.id) || "";
 
-        // si la tarea ya existe se muestra un alerta
-        if (task_find !== "") {
-            alert("La tarea que desea ingresar ya existe");
-        } else { // si no existe la agregamos al localsorage
-            array_local.unshift(object_task);
-            save_task_localstorage(array_local);
-        }
-    // si el array aún no existe en el localstorage se agrega el elemento al array_new_task declarado y luego este array se agrega al localstorage
-    } else {
-        array_new_task.unshift(object_task);
-        save_task_localstorage(array_new_task);
+    // buscamos si ya existe la tarea ingresada en el array local
+    let task_find = array_local.find((task) => object_task.id === task.id) || "";
+
+    // si la tarea ya existe se muestra un alerta
+    if (task_find !== "") {
+        // alert("La tarea que desea ingresar ya existe");
+        icon_inform.style.backgroundImage = "url(./icons/icon_cross.svg)"
+        message_modal_inform.textContent = "La tarea que desea ingresar ya existe";
+        container_modal_inform.classList.remove("ocult");
+    } else { // si no existe la agregamos al localsorage
+        array_local.unshift(object_task);
+        save_task_localstorage(array_local);
+        icon_inform.style.backgroundImage = "url(./icons/bx-check-circle.svg)"
+        message_modal_inform.textContent = "Tarea agregada correctamente";
+        container_modal_inform.classList.remove("ocult");
     }
 }
 
 // Función para guardar un array en el localstorage
 function save_task_localstorage(array) {
     localStorage.setItem("tasks", JSON.stringify(array));
+}
+
+export function show_task(container_list_task) {
+
+    container_list_task.innerHTML = "";
+    array_local = JSON.parse(localStorage.getItem("tasks")) || [];
+    console.log(array_local)
+
+    for (let i = 0; i < array_local.length; i++) {
+        const item_task = array_local[i];
+        container_list_task.innerHTML += `
+            <div class="task">
+              <input type="checkbox" class="check_task" id="check_${item_task.description.replaceAll(" ", "").toLowerCase()}"/><label for="check_${item_task.description.replaceAll(" ", "").toLowerCase()}" class="label_check"></label>
+              <p class="description_task">${item_task.description}</p>
+              <div class="container_icons_task">
+                <i class="fa-regular fa-pen-to-square icon_task icon_task_update"></i>
+                <i class="fa-solid fa-trash-can icon_task icon_task_delete"></i>
+              </div>
+            </div>`
+    }
+}
+
+export function delete_task(id_task,container_list_task_process){
+    array_local = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    const task_find = array_local.find((task)=>task.id === id_task.replaceAll(" ", "").toLowerCase());
+    const index_task = array_local.indexOf(task_find);
+
+    array_local.splice(index_task,1);
+
+    save_task_localstorage(array_local);
+    show_task(container_list_task_process);
 }
 
 
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-import {get_date,validate_empty_fields,add_task} from "./functions.js";
+import { get_date, validate_empty_fields, add_task, show_task, delete_task } from "./functions.js";
 
-window.addEventListener("DOMContentLoaded",()=>{
+window.addEventListener("DOMContentLoaded", () => {
     // elementos donde se muestra la fecha
     const number_day = document.querySelector(".number_day");
     const number_month = document.querySelector(".number_month");
@@ -9,19 +9,64 @@ window.addEventListener("DOMContentLoaded",()=>{
     // elementos para agregar tarea
     const input_add_task = document.querySelector(".input_add_task");
     const icon_add = document.querySelector(".icon_add");
+    // elementos de los modales
+    const icon_close_modal = document.querySelectorAll(".icon_close_modal");
+    const container_modal = document.querySelectorAll(".container_modal");
+    // elementos del modal de información
+    const container_modal_inform = document.querySelector(".container_modal_inform");
+    const message_modal_inform = document.querySelector(".message_modal_inform");
+    const icon_inform = document.querySelector(".icon_inform");
+    const btn_modal_inform = document.querySelector(".btn_modal_inform");
+    // elementos para mostrar las tareas
+    const container_list_task_completed = document.querySelector(".container_list_task_completed");
+    const container_list_task_process = document.querySelector(".container_list_task_process");
+    // elemento para eliminar tarea
+    // elementos del modal de confirmación
+    const container_modal_confirm = document.querySelector(".container_modal_confirm");
 
-    get_date(number_day,number_month,number_year,week_day);
-    
-    icon_add.addEventListener("click",()=>{
-        if(validate_empty_fields(input_add_task)){
-            alert("el campo de entrada esta vacio");
-        }else{
-            add_task(input_add_task);
+
+    get_date(number_day, number_month, number_year, week_day);
+    show_task(container_list_task_process)
+
+    icon_add.addEventListener("click", () => {
+        if (validate_empty_fields(input_add_task)) {
+            // alert("el campo de entrada esta vacio");
+            icon_inform.style.backgroundImage = "url(./icons/icon_cross.svg)";
+            message_modal_inform.textContent = "El campo de entrada esta vacío";
+            container_modal_inform.classList.remove("ocult");
+        } else {
+            add_task(input_add_task, icon_inform, message_modal_inform, container_modal_inform);
             input_add_task.value = "";
+            show_task(container_list_task_process);
         }
+        events_delete_update()
     })
 
-    
+    icon_close_modal.forEach((icon) => {
+        icon.addEventListener("click", () => {
+            container_modal.forEach((container) => {
+                container.classList.add("ocult");
+            })
+        })
+    })
+
+    btn_modal_inform.addEventListener("click", () => {
+        container_modal_inform.classList.add("ocult");
+    })
+
+    function events_delete_update() {
+        const icon_task_delete = document.querySelectorAll(".icon_task_delete");
+        icon_task_delete.forEach((icon) => {
+            icon.addEventListener("click", (e) => {
+                
+                let id_task = e.currentTarget.parentNode.previousElementSibling.textContent;
+                delete_task(id_task,container_list_task_process);
+                events_delete_update();
+            })
+        })
+    }
+    events_delete_update();
+
     /*
     archivos: index.js(elementos, arrays y eventos)  -  service.js(funciones)
     */
